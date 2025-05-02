ba5279c524083b862e5f4000ce2e88feba54e466
iaminfinityiq <133767092+iaminfinityiq@users.noreply.github.com>
2025-05-02 21:16:47 +0700

    VARIABLES ARE IN

--- nutshell/main.go
+++ nutshell/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"nutshell/backend/interpreter"
+	"nutshell/backend/objects"
 	"nutshell/frontend/lexer"
 	"nutshell/frontend/parser"
 	"nutshell/runtime"
@@ -9,8 +11,9 @@ import (
 )
 
 func main() {
-	var file_extension string = "nut"
-	var test string = `(1 + 2) * 3`
+	var file_extension string = "nutsh"
+	var test string = `let a = 4
+a`
 
 	var l *lexer.Lexer = lexer.InitLexer("nutshell", file_extension, test)
 	var rt runtime.RuntimeResult[*[]*lexer.Token] = l.Tokenize()
@@ -19,22 +22,41 @@ func main() {
 		return
 	}
 
+	var rt2 runtime.RuntimeResult[*parser.Block]
 	switch file_extension {
 	case "nut":
 		var nut_parser *parser.NutParser = parser.InitNutParser(rt.Result)
-		var rt runtime.RuntimeResult[*parser.Block] = nut_parser.ParseBlock()
-		if rt.Error != nil {
-			rt.Error.DisplayError()
+		rt2 = nut_parser.ParseBlock()
+		if rt2.Error != nil {
+			rt2.Error.DisplayError()
 			return
 		}
-
-		spew.Dump(rt.Result)
 	case "nutsh":
 		var nutsh_parser *parser.NutshParser = parser.InitNutshParser(rt.Result)
-		var rt runtime.RuntimeResult[*parser.Block] = nutsh_parser.ParseBlock()
-		if rt.Error != nil {
-			rt.Error.DisplayError()
+		rt2 = nutsh_parser.ParseBlock()
+		if rt2.Error != nil {
+			rt2.Error.DisplayError()
 			return
 		}
 	}
+
+	var heap *objects.Heap = &objects.Heap{
+		Heap: make(map[int]*objects.Object),
+		Last: 0,
+	}
+
+	var scope *objects.Scope = &objects.Scope{
+		Parent:      nil,
+		Heap:        heap,
+		Scope:       make(map[string]int),
+		ConstantMap: make(map[string]bool),
+	}
+
+	var rt3 runtime.RuntimeResult[*objects.Object] = interpreter.EvaluateBlock(heap, scope, rt2.Result)
+	if rt3.Error != nil {
+		rt3.Error.DisplayError()
+		return
+	}
+
+	spew.Dump(rt3.Result)
 }
