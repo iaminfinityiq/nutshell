d8a2b57e955edceddfd18fa91d7dcaf10357ddbd
iaminfinityiq <133767092+iaminfinityiq@users.noreply.github.com>
2025-05-05 23:42:10 +0700

    STATIC TYPE

--- nutshell/backend/objects/scopes.go
+++ nutshell/backend/objects/scopes.go
@@ -1,8 +1,22 @@
 package objects
 
 type Scope struct {
-	Heap  *Heap
-	Scope map[string]int
+	Parent      *Scope
+	Heap        *Heap
+	Scope       map[string]int
+	ConstantMap map[string]bool
+	DataTypeMap map[string]string
+}
+
+func (s *Scope) Declare(variable_name string, value *Object, constant bool) bool {
+	_, ok := s.Scope[variable_name]
+	if ok {
+		return false
+	}
+
+	s.Assign(variable_name, value)
+	s.ConstantMap[variable_name] = constant
+	return true
 }
 
 func (s *Scope) Assign(variable_name string, value *Object) {
@@ -10,6 +24,21 @@ func (s *Scope) Assign(variable_name string, value *Object) {
 }
 
 func (s *Scope) Access(variable_name string) (*Object, bool) {
+	heap_index, ok := s.Scope[variable_name]
+	if !ok {
+		if s.Parent == nil {
+			return nil, false
+		}
+
+		a, b := s.Parent.Access(variable_name)
+		return a, b
+	}
+
+	returned, ok := s.Heap.Heap[heap_index]
+	return returned, ok
+}
+
+func (s *Scope) ObjectAccess(variable_name string) (*Object, bool) {
 	heap_index, ok := s.Scope[variable_name]
 	if !ok {
 		return nil, false
